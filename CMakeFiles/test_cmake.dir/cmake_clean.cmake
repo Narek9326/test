552f8b243cfbb3645a4757b353edb9f87ea508f2
narek <narekhakobyan9326@gmail.com>
2018-08-24 06:44:10 -0400

    First test for company

---
+++ CMakeFiles/test_cmake.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/test_cmake.dir/test.cpp.o"
+  "test_cmake.pdb"
+  "test_cmake"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/test_cmake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
